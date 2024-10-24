2f31db839aca9da5385aa3cbe4463a76732dfc5e
Owen <oreed2@jhu.edu>
2024-10-24 12:27:01 -0400

    unholy

--- HW/HW4/hw4q3.py
+++ HW/HW4/hw4q3.py
@@ -15,43 +15,56 @@ with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
     bmr = defaultdict(lambda: (None, 0))
     bml = defaultdict(lambda: (None, 0))
     
-    for line in infile:
+    #get BMR and BML dicts 
+    for line in infile.readlines():
         parts = line.strip().split()
         if len(parts) == 3:
             id_a, overlap, id_b = parts
-            bmr[id_a] = (id_b, int(overlap))
-            if bml[id_b][1] < int(overlap):
+            #setup BMRs
+            if id_a not in bmr.keys():
+                bmr[id_a] = (id_b, int(overlap))
+            if id_b not in bml:
                 bml[id_b] = (id_a, int(overlap))
-            elif bml[id_b][1] == int(overlap) and bml[id_b][0]>id_a:
-                bml[id_b] = (None, int(overlap))
+            else:
+                #ensures BMLs are only true BMLs
+                if bml[id_b][1] < int(overlap):
+                    bml[id_b] = (id_a, int(overlap))
+                elif bml[id_b][1] == int(overlap):
+                    bml[id_b] = (None, int(overlap))
 
     unitigs = []
+    
+    attached = set()
+    non_leading = set()
 
-    seen = set()
-
+    #pre process elements of chains
     for bmr_key in bmr:
-        if bmr_key in seen:
+        bml_of_bmr = bml[bmr[bmr_key][0]][0]
+        if bmr_key == bml_of_bmr:
+            non_leading.add(bmr[bmr_key][0])
+            attached.add(bmr_key)
+            
+    # Assemble chains
+    used = set()  
+    for bmr_key in sorted(list(bmr.keys())):
+        if bmr_key in used or bmr_key in non_leading or not bmr_key in attached:
             continue
-        
+
         current_unitig = []
         current_read = bmr_key
         
-        seen.add(current_read)
-        
-        if current_read != None and current_read not in bml: #confirm that current read is not in the middle of a chain
-            current_unitig.append(current_read)
-            while current_read in bmr: #follow the rest of the chain
-                next_read, overlap_len = bmr[current_read]
-                if next_read in bml and bml[next_read][0] == current_read:
-                    current_unitig.append((overlap_len, next_read))
-                    seen.add(next_read)
-                    current_read = next_read
-                else:
-                    break
-            unitigs.append(current_unitig)
+        current_unitig.append(current_read)
+        while current_read in bmr: #follow the rest of the chain
+            next_read, overlap_len = bmr[current_read]
+            if next_read in bml and bml[next_read][0] == current_read:
+                current_unitig.append((overlap_len,next_read))
+                used.add(next_read)
+                current_read = next_read
+            else:
+                break
+        unitigs.append(current_unitig)
 
-    unitigs.sort(key=lambda x: x[0])
-
+    #print chains
     for unitig in unitigs:
         outfile.write(f"{unitig[0]}\n")
         for overlap_len, read_id in unitig[1:]:
